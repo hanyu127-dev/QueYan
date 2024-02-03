f3dd318330c152344ef9e818395848c3873823ed
启明 <3220264629@qq.com>
2024-02-03 19:50:48 +0800

    初始化事件，登陆，最小化，退出，屏幕移动

--- chat-ui-01/src/main/java/com/hanyu/chat/ui/view/Login.java
+++ chat-ui-01/src/main/java/com/hanyu/chat/ui/view/Login.java
@@ -1,8 +1,12 @@
 package com.hanyu.chat.ui.view;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -24,6 +28,59 @@ public class Login extends Stage {
         // 设置窗体能否被用户调整
         setResizable(false);
         this.getIcons().add(new Image("/fxml/login/img/system/logo.png"));
+        initEvent();
 
+    }
+    private void  initEvent(){
+        move();
+        min();
+        quit();
+        login();
+    }
+
+    private Double xOffset;
+    private Double yOffset;
+
+    private void move(){
+        root.setOnMousePressed(event -> {
+            xOffset = getX() - event.getScreenX();
+            yOffset = getY() - event.getScreenY();
+            root.setCursor(Cursor.CLOSED_HAND);
+        });
+        root.setOnMouseDragged(event -> {
+            setX(event.getScreenX() + xOffset);
+            setY(event.getScreenY() + yOffset);
+        });
+        root.setOnMouseReleased(event -> {
+            root.setCursor(Cursor.DEFAULT);
+        });
+    }
+
+    private void min(){
+        Button loginMin = getUI("login_min", Button.class);
+        loginMin.setOnAction(event -> {
+            setIconified(true);
+        });
+    }
+    private void quit(){
+        Button loginClose = getUI("login_close", Button.class);
+        loginClose.setOnAction(event -> {
+            close();
+            System.exit(0);
+        });
+    }
+
+    private void login(){
+        TextField userId = getUI("userId",TextField.class);
+        PasswordField password = getUI("userPassword",PasswordField.class);
+        getUI("login_button", Button.class).setOnAction(event -> {
+            System.out.println("登陆操作");
+            System.out.println("用户ID：" + userId.getText());
+            System.out.println("用户密码：" + password.getText());
+        });
+    }
+
+    private <T> T getUI(String id,Class<T> clazz){
+        return (T)root.lookup("#"+id);
     }
 }
